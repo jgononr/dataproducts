11e78de67cc46842111972b6015afea507580f81
Javier González <xavigo@gmail.com>
2018-04-22 21:52:42 +0200

    Intermediate commit

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -10,25 +10,106 @@
 library(shiny)
 library(DT)
 library(ISLR)
+library(caret)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
-    model <- lm(mpg~horsepower, data = Auto)
+    trainset <- reactive({
+        sel <- input$config_trainsetselection
+        percent <- input$config_trainsetpercent
 
-    output$plot <- renderPlot({
-        plot(y = Auto$mpg, x = Auto$horsepower,
-             ylab = "Miles per Gallon", xlab = "Horsepower")
-        abline(model, col = "red", lwd = 2)
+        if(sel == 1) {
+            ## First n% elements
+            selectedrow <- round(nrow(Auto)*percent/100)
+            rows <- c(1:selectedrow)
+        } else if (sel == 2) {
+            ## Middle n%elements
+            trainsetelements <- round(nrow(Auto)*(100 - percent)/2)/100
+            rows <- c(trainsetelements:(nrow(Auto)-trainsetelements))
+        } else if (sel == 3) {
+            ## Last n% elements
+            selectedrow <- nrow(Auto) - round(nrow(Auto)*percent/100)
+            rows <- c(selectedrow:nrow(Auto))
+        } else {
+            ## Create data partitions
+            rows <- createDataPartition(y = Auto$mpg, p = percent/100,
+                                        list = FALSE)
+        }
+        rows
     })
 
-    output$sl_hp <- ({renderText(input$ti_hp)})
+    plotmodel <- reactive({
+        model.lm <- lm(mpg~horsepower, data = Auto[trainset(),])
+        model.ss <- smooth.spline(Auto[trainset(), ]$horsepower,
+                      Auto[trainset(), ]$mpg,
+                      df = input$model_ss.df, nknots = input$model_ss.knots)
+        model1.lm3 <- lm(mpg ~ poly(horsepower, degree=input$model1_poly.degree),
+                        data=Auto)
+        model2.lm3 <- lm(mpg ~ poly(horsepower, degree=input$model2_poly.degree),
+                         data=Auto)
+        polydata1 <- polydata2 <- data.frame(horsepower = seq(
+                                                min(df$horsepower),
+                                                max(df$horsepower),
+                                                length.out = 100))
+        polydata1$mpg <- predict(model1.lm3, newdata = modelplot)
+        polydata2$mpg <- predict(model2.lm3, newdata = modelplot)
+        if (input$model1_plot) {
+            sel <- input$model1_modelsel
+            if (sel == 1) {
+                ## Linear model
+                abline(model.lm,
+                       col = input$model1_colorsel, lwd = input$model1_width,
+                       lty = input$model1_linetype)
+            } else if (sel == 2) {
+                ## Polynomial regression
+                lines(polydata1$horsepower, polydata1$mpg,
+                      col = input$model1_colorsel, lwd = input$model1_width,
+                      lty = input$model1_linetype)
+            } else {
+                ## Smooth spline
+                lines(model.ss,
+                      col = input$model1_colorsel, lwd = input$model1_width,
+                      lty = input$model1_linetype)
+            }
+        }
+        if (input$model2_plot) {
+            sel <- input$model2_modelsel
+            if (sel == 1) {
+                ## Linear model
+                abline(model.lm,
+                       col = input$model2_colorsel, lwd = input$model2_width,
+                       lty = input$model2_linetype)
 
-    output$ti_hp <- ({renderText(input$sl_hp)})
+            } else if (sel == 2) {
+                ## Polynomial regression
+                cat(file = stderr(), "model1_poly_change\n")
+                lines(polydata2$horsepower, polydata2$mpg,
+                      col = input$model2_colorsel, lwd = input$model2_width,
+                      lty = input$model2_linetype)
+            } else {
+                ## Smooth spline
+                lines(model.ss,
+                      col = input$model2_colorsel, lwd = input$model2_width,
+                      lty = input$model2_linetype)
+            }
+        }
+    })
+
+    output$plot <- renderPlot({
+        df <- Auto[order(Auto$horsepower),]
+        plot(y = df[trainset(),]$mpg, x = df[trainset(),]$horsepower,
+             ylim = c(0, max(Auto$mpg)), xlim = c(0, max(Auto$horsepower)),
+             col = input$config_trainsetcolor,
+             ylab = "Miles per Gallon", xlab = "Horsepower")
+        points(y = df[-trainset(),]$mpg, x = df[-trainset(),]$horsepower,
+             col = input$config_testsetcolor)
+        plotmodel()
+    })
 
     output$dataset <- DT::renderDataTable({
         DT::datatable({Auto})
-})
+    })
 
-  ## session$onSessionEnded(stopApp)
+    session$onSessionEnded(stopApp)
 })
--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -8,36 +8,212 @@
 #
 
 library(shiny)
+library(ISLR)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-  # Application title
-  titlePanel("Modelling Shiny App"),
+    # Application title
+    titlePanel("Modelling Shiny App"),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(position = "right", fluid = FALSE,
-    sidebarPanel(
-        tabsetPanel(
-            tabPanel("Model",
-                     sliderInput("sl_hp","Horsepower",
-                                 min = 0, max = max(Auto$horsepower),
-                                 value = 0),
-                     textInput("ti_hp", "HP")
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(position = "right", fluid = FALSE,
+        sidebarPanel(
+            tabsetPanel(
+                tabPanel("Model 1",
+                         selectInput("model1_modelsel",
+                                     label = "Model selection",
+                                     choices = list(
+                                         "Linear" = 1,
+                                         "Polynomial" = 2,
+                                         "Smooth spline" = 3),
+                                     selected = 1
+                         ),
+                         h4("Regression parameters"),
+                         conditionalPanel(
+                             condition = "input.model1_modelsel == '1'",
+                             helpText("There are no specific parameters",
+                                      "for this model")
+                         ),
+                         conditionalPanel(
+                             condition = "input.model1_modelsel == '2'",
+                             helpText("Use the slider to select the polynomial",
+                                      "degree. E.g. '1' for linear regression",
+                                      "'2' for quadratic, '3' for cubic..."),
+                             sliderInput("model1_poly.degree",
+                                         label = "Polynomial degree",
+                                         min = 1, max = 10, value = 2)
+                         ),
+                         conditionalPanel(
+                             condition = "input.model1_modelsel == '3'",
+                             sliderInput("model_ss.df",
+                                         label = "Degrees of Freedom",
+                                         min = 1, max = 10, value = 5),
+                             sliderInput("model_ss.nknots",
+                                         label = "Number of knots",
+                                         min = 1, max = 10, value = 5)
+                         ),
+                         h4("Plot options"),
+                         helpText("Use these controls to specify the regression",
+                                  "line color, line style and width."),
+                         selectInput("model1_colorsel",
+                                     label = "Model color",
+                                     choices = list(
+                                         "Blue" = "blue",
+                                         "Red" = "red",
+                                         "Green" = "green",
+                                         "Yellow" = "yellow"
+                                     ), selected = "red"),
+                         sliderInput("model1_width",
+                                     label = "Line width",
+                                     min = 1, max = 10, value = 2),
+                         selectInput("model1_linetype",
+                                     label = "Line type",
+                                     choices = list(
+                                         "Solid" = "solid",
+                                         "Dashed" = "dashed",
+                                         "Dotted" = "dotted",
+                                         "Dot-Dash" = "dotdash",
+                                         "Long Dash" = "longdash",
+                                         "2-dash" = "twodash"
+                                     ),
+                                     selected = "solid"),
+                         checkboxInput("model1_plot", "Plot", value = TRUE)
+                ),
+                tabPanel("Model 2",
+                         selectInput("model2_modelsel",
+                                     label = "Model selection",
+                                     choices = list(
+                                         "Linear" = 1,
+                                         "Polynomial" = 2,
+                                         "Smooth spline" = 3),
+                                     selected = 2
+                         ),
+                         h4("Regression parameters"),
+                         conditionalPanel(
+                             condition = "input.model2_modelsel == '1'",
+                             helpText("There are no specific parameters",
+                                      "for this model")
+                         ),
+                         conditionalPanel(
+                             condition = "input.model2_modelsel == '2'",
+                             helpText("Use the slider to select the polynomial",
+                                      "degree. E.g. '1' for linear regression",
+                                      "'2' for quadratic, '3' for cubic..."),
+                             sliderInput("model2_poly.degree",
+                                         label = "Polynomial degree",
+                                         min = 1, max = 10, value = 2)
+                         ),
+                         conditionalPanel(
+                             condition = "input.model2_modelsel == '3'",
+                             sliderInput("model_ss.df",
+                                         label = "Degrees of Freedom",
+                                         min = 1, max = 4, value = 2),
+                             sliderInput("model_ss.nknots",
+                                         label = "Number of knots",
+                                         min = 1, max = 10, value = 5)
+                         ),
+                         h4("Plot options"),
+                         helpText("Use these controls to specify the regression",
+                                  "line color, line style and width."),
+                         selectInput("model2_colorsel",
+                                     label = "Model color",
+                                     choices = list(
+                                         "Blue" = "blue",
+                                         "Red" = "red",
+                                         "Green" = "green",
+                                         "Yellow" = "yellow"
+                                     ), selected= "green"),
+                         sliderInput("model2_width",
+                                     label = "Line width",
+                                     min = 1, max = 10, value = 2),
+                         selectInput("model2_linetype",
+                                     label = "Line type",
+                                     choices = list(
+                                         "Solid" = "solid",
+                                         "Dashed" = "dashed",
+                                         "Dotted" = "dotted",
+                                         "Dot-Dash" = "dotdash",
+                                         "Long Dash" = "longdash",
+                                         "2-dash" = "twodash"
+                                     ),
+                                     selected = "solid"),
+                         checkboxInput("model2_plot", "Plot", value = TRUE)
+                ),
+                tabPanel("Configuration",
+                    h4("Training set"),
+                    helpText("Please select which part and percentage",
+                             "of the available data in the dataset will be",
+                             "used for model training"),
+                    selectInput("config_trainsetselection",
+                                label = "Set selection",
+                                choices = list(
+                                    "First % data" = 1,
+                                    "Middle % data" = 2,
+                                    "Last % data" = 3,
+                                    "Random data" = 4),
+                                selected = 4
                      ),
-            tabPanel("Panel 2", "Content 2"),
-            tabPanel("Panel 3", "Content 3")
-        )
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-        tabsetPanel(
-            tabPanel("Data", DT::dataTableOutput("dataset")),
-            tabPanel("Plot",
-                     plotOutput("plot")
+                    conditionalPanel(
+                        condition = "input.config_trainsetselection == '1'",
+                        helpText("The left-hand percentage (ordered by horsepower)",
+                                 "of the data set will be used for regression",
+                                 "model calculation")
+                    ),
+                    conditionalPanel(
+                        condition = "input.config_trainsetselection == '2'",
+                        helpText("A centered percentage (ordered by horsepower)",
+                                 "of the data set will be used for regression",
+                                 "model calculation")
+                    ),
+                    conditionalPanel(
+                        condition = "input.config_trainsetselection == '3'",
+                        helpText("The right-hand percentage (ordered by",
+                                 "horsepower)",
+                                 "of the data set will be used for regression",
+                                 "model calculation")
+                    ),
+                    conditionalPanel(
+                        condition = "input.config_trainsetselection == '4'",
+                        helpText("A random percentage",
+                                 "of the data set will be used for regression",
+                                 "model calculation")
+                    ),
+                    sliderInput("config_trainsetpercent",
+                              label = "Available data percentage",
+                              min = 1, max = 99, value = 75),
+                    selectInput("config_trainsetcolor",
+                                label = "Training data color",
+                                choices = list(
+                                    "Black" = "black",
+                                    "Blue" = "blue",
+                                    "Red" = "red",
+                                    "Green" = "green",
+                                    "Yellow" = "yellow"),
+                                selected = "black"
+                    ),
+                    h4("Test set"),
+                    selectInput("config_testsetcolor",
+                                label = "Test data color",
+                                choices = list(
+                                    "Black" = "black",
+                                    "Blue" = "blue",
+                                    "Red" = "red",
+                                    "Green" = "green",
+                                    "Yellow" = "yellow"),
+                                selected = "blue"
+                    )
+                )
+            )
+        ),
+        # Show a plot of the generated distribution
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Plot",
+                         plotOutput("plot")
+                ),
+                tabPanel("Data", DT::dataTableOutput("dataset"))
             )
         )
     )
-  )
 ))
